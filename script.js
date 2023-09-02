1cef4d44ce458a4ce9f6699ac036e0edbc073b79
munarabaisalova <115396498+munarabaisalova@users.noreply.github.com>
2023-09-02 19:52:59 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let block = document.getElementById("block")
+let content = document.getElementById("content")
+let cont = document.getElementById("cont")
+
+
+let link = document.createElement("h1")
+cont.innerHTML =('7:25 AM');
+
+cont.style.color ="blue"
+cont.style.fontSize ="20px"
+cont.style.fontWeight = "bold"
+cont.append = (link);
+
+
+
+
+
+
+
+
+
+cont1.innerHTML =('8:15 PM');
+cont1.style.color ="blue"
+cont1.style.fontSize ="20px"
+cont1.style.fontWeight = "bold"
+cont1.append = (link)
+
+cont2.innerHTML =("Translate");
+cont2.style.color ="blue"
+cont2.style.fontSize ="20px"
+cont2.style.fontWeight = "bold"
+cont2.append = (link)
+
+cont3.innerHTML =("Bike");
+cont3.style.color ="orange"
+cont3.style.fontSize ="20px"
+cont3.style.fontWeight = "bold"
+cont3.append = (link)
+
+cont4.innerHTML =("Boat");
+cont4.style.color ="orange"
+cont4.style.fontSize ="20px"
+cont4.style.fontWeight = "bold"
+cont4.append = (link)
+
+cont5.innerHTML =("Drive");
+cont5.style.color ="orange"
+cont5.style.fontSize ="20px"
+cont5.style.fontWeight = "bold"
+cont5.append = (link)
+
+cont6.innerHTML =("Morning");
+cont6.style.color ="red"
+cont6.style.fontSize ="20px"
+cont6.style.fontWeight = "bold"
+cont6.append = (link)
+
+cont7.innerHTML =("Afternoon");
+cont7.style.color ="red"
+cont7.style.fontSize ="20px"
+cont7.style.fontWeight = "bold"
+cont7.append = (link)
+
+cont8.innerHTML =("Night");
+cont8.style.color ="red"
+cont8.style.fontSize ="20px"
+cont8.style.fontWeight = "bold"
+cont8.append = (link)
